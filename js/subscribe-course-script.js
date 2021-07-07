4946cc81e2a48ce65d46220108e34d67d4571c58
Franco-Ou <foubelverdesco@alumno.unlam.edu.ar>
2021-07-07 00:47:41 -0300

    Agregué modal en inscripción a curso, arreglé footer y detalles varios

--- js/subscribe-course-script.js
+++ js/subscribe-course-script.js
@@ -1,49 +1,56 @@
-$(document).ready(function(){
-    let counter = 0;
+$(document).ready(function () {
+  $("#modal-inscribirse").hide();
+  let counter = 0;
 
-    $("#subscribe-user")
-    .append(
+  $("#subscribe-user").append(
     `<div id=${"element-form-" + 0} class="element-form">
-        <input type="text" placeholder="Nombre">
-        <input type="text" placeholder="Apellido">
-        <input type="text" placeholder="DNI">
+        <input type="text" placeholder="Nombre" required>
+        <input type="text" placeholder="Apellido" required>
+        <input type="text" placeholder="DNI" required>
         <span id=${0} class="btn-borrar"><i class="fas fa-minus-circle"></i></span>
-      </div>`)
+      </div>`
+  );
 
-      $('.price-subscribe').text("1000.-")
+  $(".price-subscribe").text("1000.-");
 
-
-    $(".btn").click(function(e){
-        e.preventDefault();
-        counter++;
-        if(counter < 5){
-            $("#subscribe-user")
-            .append(
-            `<div id=${"element-form-" + counter} class="element-form">
+  $(".btn").click(function (e) {
+    e.preventDefault();
+    counter++;
+    if (counter < 7) {
+      $("#subscribe-user").append(
+        `<div id=${"element-form-" + counter} class="element-form">
                 <input type="text" placeholder="Nombre">
                 <input type="text" placeholder="Apellido">
                 <input type="text" placeholder="DNI">
                 <span id=${counter} class="btn-borrar"><i class="fas fa-minus-circle"></i></span>
-              </div>`)
+              </div>`
+      );
 
-              $('.price-subscribe').text(counter+1 + "000.-")
-        }
-    })
-
-    function borrarElemento(id){
-        if(counter >= 1 ){
-            $("#element-form-" + id).remove();
-            counter--;
-            let contador = counter;
-            $('.price-subscribe').text(contador+1 + "000.-")
-        }
-
+      $(".price-subscribe").text(counter + 1 + "000.-");
     }
+  });
 
-    $('#subscribe-user').on('click', '.btn-borrar', function(e) {
-        var id = $(this).attr('id');
-        borrarElemento(id)
-    })
+  function borrarElemento(id) {
+    if (counter >= 1) {
+      $("#element-form-" + id).remove();
+      counter--;
+      let contador = counter;
+      $(".price-subscribe").text(contador + 1 + "000.-");
+    }
+  }
 
+  $("#subscribe-user").on("click", ".btn-borrar", function (e) {
+    var id = $(this).attr("id");
+    borrarElemento(id);
+  });
 
-})
+  $("#btn-subscribe").click(function (e) {
+    e.preventDefault();
+    $("#modal-inscribirse").fadeIn();
+    $("#monto-modal").text("Monto a pagar: $" + (counter + 1) + "000");
+  });
+
+  $("#modal-inscribirse").click(function () {
+    $("#modal-inscribirse").fadeOut();
+  });
+});
